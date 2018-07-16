74289d533ac7ce4a9be654c83b3008cf6134f3f7
Pritin Tyagaraj <pritin.tyagaraj@klarna.com>
2018-07-17 00:04:12 +0200

    Introduce dependency 'restify'

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
-console.log('API!')
+const restify = require('restify')
+
+const respond = (req, res, next) => {
+  res.send('Tjena!')
+  next()
+}
+
+const server = restify.createServer()
+server.get('/', respond)
+server.listen(8080, () => {
+  console.log('Listening...')
+})
